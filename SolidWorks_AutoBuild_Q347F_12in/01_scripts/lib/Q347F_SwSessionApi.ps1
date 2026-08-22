519ea6b50bcb9da1c69377df6451a22451fdfe2f
Eleanor Thompson <447015507@qq.com>
2026-08-22 14:17:47 +0800

    feat(sw-autobuild): add SolidWorks session COM layer

---
+++ SolidWorks_AutoBuild_Q347F_12in/01_scripts/lib/Q347F_SwSessionApi.ps1
@@ -0,0 +1,74 @@
+function Add-EmbeddedSwSessionApiType {
+    if ('Q347F.SwSessionApi' -as [type]) { return }
+
+    $source = @'
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swconst;
+
+namespace Q347F
+{
+    public sealed class SwSession
+    {
+        public SldWorks App;
+        public string Mode;
+        public string Revision;
+        public int ProcessId;
+        public bool Visible;
+        public int LatestFileVersion;
+    }
+
+    public static class SwSessionApi
+    {
+        public static SwSession ConnectOrStart()
+        {
+            object obj = null;
+            string mode = "";
+            try
+            {
+                obj = Marshal.GetActiveObject("SldWorks.Application");
+                mode = "ATTACH";
+            }
+            catch
+            {
+                Type t = Type.GetTypeFromProgID("SldWorks.Application", true);
+                obj = Activator.CreateInstance(t);
+                mode = "START_NEW";
+            }
+
+            SldWorks app = (SldWorks)obj;
+            app.Visible = true;
+            return new SwSession
+            {
+                App = app,
+                Mode = mode,
+                Revision = app.RevisionNumber(),
+                ProcessId = app.GetProcessID(),
+                Visible = app.Visible,
+                LatestFileVersion = app.GetLatestSupportedFileVersion()
+            };
+        }
+
+        public static string GetDefaultPartTemplate(SldWorks app)
+        {
+            return app.GetUserPreferenceStringValue((int)swUserPreferenceStringValue_e.swDefaultTemplatePart);
+        }
+
+        public static ModelDoc2 NewPart(SldWorks app, string templatePath)
+        {
+            object doc = app.NewDocument(templatePath, 0, 0.0, 0.0);
+            if (doc == null) throw new InvalidOperationException("ISldWorks.NewDocument returned null.");
+            ModelDoc2 model = (ModelDoc2)doc;
+            model.ShowFeatureErrorDialog = false;
+            return model;
+        }
+    }
+}
+
+'@
+
+    Add-Type -TypeDefinition $source -Language CSharp -ReferencedAssemblies @($script:InteropSldworks, $script:InteropSwconst, 'System.dll', 'System.Core.dll')
+}
